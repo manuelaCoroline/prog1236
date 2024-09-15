d4c8da30d8a1c5a534dc4660958e80930f092b39
Dylane <tanolamene@gmail.com>
2024-09-15 07:55:08 -0300

    Types de manga

---
+++ TypesMangaForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TypesManga
+{
+    public partial class TypesMangaForm : Form
+    {
+        public TypesMangaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void classificationLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
